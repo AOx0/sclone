062f5995cdb7e436dc14aebabc3de30040b322bc
AOx0 <50227494+AOx0@users.noreply.github.com>
2021-12-09 03:02:38 -0600

    File auto reformat

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 extern crate execute;
 extern crate fs_extra;
 
-use fs_extra::dir::{move_dir};
+use fs_extra::dir::move_dir;
 
+use execute::{command as c, Execute};
 use std::process::{Command, Stdio};
-use execute::{Execute, command as c};
 
 use clap::{AppSettings, Parser};
-use std::fs::{OpenOptions};
+use fs_extra::file::move_file;
 use std::env::{current_dir, set_current_dir};
+use std::fs::OpenOptions;
 use std::io::Write;
 use std::ops::Add;
-use fs_extra::file::move_file;
-
 
 #[derive(Parser, Debug)]
 #[clap(about, version, author, setting = AppSettings::DisableColoredHelp)]
@@ -34,7 +33,7 @@ struct Args {
     in_place: bool,
 }
 
-pub fn write_contents_to(path: &str, contents: &[u8]) -> std::io::Result<()>  {
+pub fn write_contents_to(path: &str, contents: &[u8]) -> std::io::Result<()> {
     let mut file = OpenOptions::new()
         .read(true)
         .write(true)
@@ -46,15 +45,13 @@ pub fn write_contents_to(path: &str, contents: &[u8]) -> std::io::Result<()>  {
     Ok(())
 }
 
-
-
 fn main() -> std::io::Result<()> {
     let args = Args::parse();
 
     let name: String = args.url.clone();
     let name = name.split("/").collect::<Vec<&str>>()[4];
 
-    let name = if args.in_place{
+    let name = if args.in_place {
         format!("temp_{}", name)
     } else {
         name.to_string()
@@ -62,11 +59,9 @@ fn main() -> std::io::Result<()> {
 
     let original_wk_directory: String = format!("{}", current_dir()?.display());
 
-
     let wk_directory = format!("{}/{}", current_dir()?.display(), name);
 
     fs_extra::dir::create(name, args.in_place).expect("Failed to create directory");
-
 
     set_current_dir(wk_directory.clone()).expect("Failed to set working directory");
 
@@ -78,53 +73,58 @@ fn main() -> std::io::Result<()> {
     let output = command.execute_output().unwrap();
 
     if let Some(exit_code) = output.status.code() {
-        if !(exit_code == 0 &&
-            String::from_utf8(output.stdout)
+        if !(exit_code == 0
+            && String::from_utf8(output.stdout)
                 .expect("Failed to get git init output")
-                .contains("Initialized empty Git repository in")
-        ) {
+                .contains("Initialized empty Git repository in"))
+        {
             eprintln!("Failed to initialize empty github repo");
         }
-    } else { eprintln!("Interrupted!"); }
+    } else {
+        eprintln!("Interrupted!");
+    }
 
     let mut command = c(format!("git remote add -f origin {}", args.url));
 
     if let Some(exit_code) = command.execute().unwrap() {
-        if !(exit_code == 0)  {  eprintln!("Failed to add remote repo url"); }
-    } else { eprintln!("Interrupted!"); }
+        if !(exit_code == 0) {
+            eprintln!("Failed to add remote repo url");
+        }
+    } else {
+        eprintln!("Interrupted!");
+    }
 
     let mut command = c("git config core.sparseCheckout true");
 
     if let Some(exit_code) = command.execute().unwrap() {
-        if !(exit_code == 0) { eprintln!("Failed to write to .git/config"); }
+        if !(exit_code == 0) {
+            eprintln!("Failed to write to .git/config");
+        }
     } else {
         eprintln!("Interrupted!");
     }
 
     for folder in args.folders {
-        write_contents_to(format!("{}/.git/info/sparse-checkout", wk_directory.clone()).as_str(), folder.add("\n").as_bytes())?
+        write_contents_to(
+            format!("{}/.git/info/sparse-checkout", wk_directory.clone()).as_str(),
+            folder.add("\n").as_bytes(),
+        )?
     }
 
     let mut command = c(format!("git pull origin {}", args.branch));
-
-
 
     if let Some(exit_code) = command.execute().unwrap() {
         if !(exit_code == 0) && args.branch == "main" {
             let mut command = c(format!("git pull origin master"));
             if let Some(exit_code) = command.execute().unwrap() {
                 if !(exit_code == 0) && args.branch == "main" {
-                    eprintln!("Failed to get folders from repo");
+                    eprintln!("Failed to get folders from repo. Check the branch name is correct.");
                 }
             } else {
-
                 eprintln!("Interrupted!");
             }
-
-
         }
     } else {
-
         eprintln!("Interrupted!");
     }
 
@@ -133,15 +133,26 @@ fn main() -> std::io::Result<()> {
         let options_file = fs_extra::file::CopyOptions::new();
 
         for entry in std::fs::read_dir(".")? {
-            let e =  entry.expect("Failed to read dir files");
-            if e.file_name() == ".git" { continue };
+            let e = entry.expect("Failed to read dir files");
+            if e.file_name() == ".git" {
+                continue;
+            };
 
             if e.file_type().unwrap().is_dir() {
-                move_dir(e.path(), original_wk_directory.clone(), &options_dir).expect("Failed to copy dir");
+                move_dir(e.path(), original_wk_directory.clone(), &options_dir)
+                    .expect("Failed to copy dir");
             } else {
-                move_file(e.path(), format!("{}/{}", original_wk_directory.clone(), e.file_name().into_string().unwrap()), &options_file).expect("Failed to copy dir");
+                move_file(
+                    e.path(),
+                    format!(
+                        "{}/{}",
+                        original_wk_directory.clone(),
+                        e.file_name().into_string().unwrap()
+                    ),
+                    &options_file,
+                )
+                .expect("Failed to copy dir");
             }
-
         }
 
         fs_extra::dir::remove(wk_directory.clone()).expect("Failed to remove dir");
