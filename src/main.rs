e96501ecc9ed1b2c8f1a04d54f073ebfee458681
AOx0 <50227494+AOx0@users.noreply.github.com>
2021-12-09 01:21:45 -0600

    Init

---
+++ src/main.rs
@@ -0,0 +1,140 @@
+extern crate execute;
+extern crate fs_extra;
+
+use fs_extra::dir::{CopyOptions, move_dir};
+
+use std::process::{Command, Stdio};
+use execute::{Execute, command as c};
+
+use clap::{AppSettings, Parser};
+use std::fs::{create_dir, OpenOptions};
+use std::env::{current_dir, set_current_dir};
+use std::io::Write;
+use std::ops::Add;
+
+
+#[derive(Parser, Debug)]
+#[clap(about, version, author, setting = AppSettings::DisableColoredHelp)]
+struct Args {
+    //Url of the github repository
+    #[clap(required = true)]
+    url: String,
+
+    // Folders to clone from main/
+    #[clap(required = true, multiple_values = true)]
+    folders: Vec<String>,
+
+    // Name of the branch to clone
+    #[clap(short, long, required = false, default_value = "main")]
+    branch: String,
+
+    // If you want to store in place cloned files
+    #[clap(short, long, takes_value = false)]
+    in_place: bool,
+}
+
+pub fn write_contents_to(path: &str, contents: &[u8]) -> std::io::Result<()>  {
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .append(true)
+        .open(path)?;
+
+    file.write(contents)?;
+    Ok(())
+}
+
+
+
+fn main() -> std::io::Result<()> {
+    let args = Args::parse();
+
+    let name: String = args.url.clone();
+    let name = name.split("/").collect::<Vec<&str>>()[4];
+
+    let name = if args.in_place{
+        format!("temp_{}", name)
+    } else {
+        name.to_string()
+    };
+
+    let original_wk_directory: String = format!("{}", current_dir()?.display());
+
+
+    let wk_directory = if args.in_place{
+        format!("{}/temp_{}", current_dir()?.display(), name)
+    } else {
+        format!("{}/{}", current_dir()?.display(), name)
+    };
+
+    fs_extra::dir::create(wk_directory.clone(), false).expect("Failed to create directory");
+
+
+    set_current_dir(wk_directory.clone()).expect("Failed to set working directory");
+
+    let mut command = Command::new("git");
+    command.arg("init");
+    command.stdout(Stdio::piped());
+    command.stderr(Stdio::piped());
+
+    let output = command.execute_output().unwrap();
+
+    if let Some(exit_code) = output.status.code() {
+        if !(exit_code == 0 &&
+            String::from_utf8(output.stdout)
+                .expect("Failed to get git init output")
+                .contains("Initialized empty Git repository in")
+        ) {
+            eprintln!("Failed to initialize empty github repo");
+        }
+    } else { eprintln!("Interrupted!"); }
+
+    let mut command = c(format!("git remote add -f origin {}", args.url));
+
+    if let Some(exit_code) = command.execute().unwrap() {
+        if !(exit_code == 0)  {  eprintln!("Failed to add remote repo url"); }
+    } else { eprintln!("Interrupted!"); }
+
+    let mut command = c("git config core.sparseCheckout true");
+
+    if let Some(exit_code) = command.execute().unwrap() {
+        if !(exit_code == 0) { eprintln!("Failed to write to .git/config"); }
+    } else {
+        eprintln!("Interrupted!");
+    }
+
+
+
+    for folder in args.folders {
+        write_contents_to(format!("{}/.git/info/sparse-checkout", wk_directory.clone()).as_str(), folder.add("\n").as_bytes())?
+    }
+
+    let mut command = c(format!("git pull origin {}", args.branch));
+
+    if let Some(exit_code) = command.execute().unwrap() {
+        if !(exit_code == 0) { eprintln!("Failed to get folders from repo"); }
+    } else {
+        eprintln!("Interrupted!");
+    }
+
+    if args.in_place {
+        let options = CopyOptions::new();
+
+        for entry in std::fs::read_dir(".")? {
+            let e =  entry.expect("Failed to read dir files");
+            if e.file_name() == ".git" { continue };
+
+            if e.file_type().is_ok() &&  e.file_type().unwrap().is_dir() {
+                move_dir(e.path(), original_wk_directory.clone(), &options).expect("Failed to copy dir");
+            } else {
+                std::fs::copy(e.path(), original_wk_directory.clone())?;
+            }
+
+        }
+
+        fs_extra::dir::remove(wk_directory.clone()).expect("Failed to remove dir");
+    }
+
+    Ok(())
+}
